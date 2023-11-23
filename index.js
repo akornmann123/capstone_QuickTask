d67ee8278ae30454a3a99fb49120b1f8f048bc97
Jrudesill <143745567+Jrudesill@users.noreply.github.com>
2023-11-22 18:25:01 -0600

    Update index.js
    Added require path

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { Pool } = require('pg');
 const fs = require('fs');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
