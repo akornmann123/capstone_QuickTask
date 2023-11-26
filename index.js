792c385d489471886dacdf179636954edcb08f2a
akornmann <akornmann@student.cvtc.edu>
2023-11-26 14:48:50 -0600

    fixing create form

--- index.js
+++ index.js
@@ -132,17 +132,17 @@ app.get('/tasks', async (req, res) => {
 
 
 // TODO 
-//app.get('/create-task', async (req, res) => {
-  //  const task = req.body; 
-    //try {
-      //await pool.query('INSERT INTO tasks (userAccounts.fName, description, due_date, assigned_to, employee_email, notes) VALUES ($1, $2, $3, $4, $5, $6)', [task.fname, task.description, task.due_date, task.assigned_to, task.employee_email, task.notes]);
-      //res.status(201).json({ message: 'Task created successfully.' });
-    //} catch (error) {
-      //console.error(error);
-      //res.status(500).json({ error: 'Internal server error.' });
-    //}
-  //});
+app.get('/create-task', async (req, res) => {
+    const task = req.body; 
+    try {
+      await pool.query('INSERT INTO tasks (userAccounts.fName, description, due_date, assigned_to, employee_email, notes) VALUES ($1, $2, $3, $4, $5, $6)', [task.fname, task.description, task.due_date, task.assigned_to, task.employee_email, task.notes]);
+      res.status(201).json({ message: 'Task created successfully.' });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Internal server error.' });
+    }
+  });
 
-//app.listen(port, () => {
-  //  console.log(`Server is running on port ${port}`);
-//});
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
