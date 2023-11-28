f6ed259778c12c1268349763bb298544c5c4c11a
akornmann123 <123998259+akornmann123@users.noreply.github.com>
2023-11-27 19:57:03 -0600

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
