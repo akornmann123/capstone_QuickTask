d34e700d979658df89208998dd5e590e149070f1
noverboe <noverboe@student.cvtc.edu>
2023-12-06 21:45:34 -0600

    Finishes [#186621068] changed login to text field

--- schema.sql
+++ schema.sql
@@ -33,6 +33,7 @@ INSERT INTO userAccounts (fName, lName, job, username, password) VALUES ('Mike',
 INSERT INTO userAccounts (fName, lName, job, username, password) VALUES ('Billy', 'Jones', 'Employee', 'username', 'password');
 INSERT INTO userAccounts (fName, lName, job, username, password) VALUES ('Chrisopher', 'Robin', 'Employee', 'username', 'password');
 INSERT INTO userAccounts (fName, lName, job, username, password) VALUES ('Sally', 'Corrin', 'Manager', 'username', 'password');
+INSERT INTO userAccounts (fName, lName, job, username, password) VALUES ('admin', 'admin', 'Manager', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918');
 
 -- insert tasks
 INSERT INTO tasks (title, description, completed, due_date, user_id, notes, status) VALUES
